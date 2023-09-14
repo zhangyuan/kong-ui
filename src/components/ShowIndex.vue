c62c52450fe54aa864d96b3db4334ce57af56f28
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-09-14 11:09:25 +0800

    add routes to home page

--- src/components/ShowIndex.vue
+++ src/components/ShowIndex.vue
@@ -4,6 +4,7 @@
 
 <template>
   <div class="box"><router-link :to="{name: 'services'}">Services</router-link></div>
+  <div class="box"><router-link :to="{name: 'routes'}">Routes</router-link></div>
   <div class="box"><router-link :to="{name: 'plugins'}">Plugins</router-link></div>
   <div class="box"><router-link :to="{name: 'node'}">Node Information</router-link></div>
   <div class="box"><router-link :to="{name: 'endpoints'}">Endpoints</router-link></div>
